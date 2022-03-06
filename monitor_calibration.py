c2d564a80b3cf4c9fbe822b1dd906548c4f218b4
ffletcherr <homayouneemoradi@gmail.com>
2022-03-06 16:52:23 +0330

    new version (fix bugs)

--- monitor_calibration.py
+++ monitor_calibration.py
@@ -6,27 +6,48 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 from screeninfo import get_monitors
+
+
 def plucker_intersection(line_point, line_dir, plane_point, plane_normal_vector):
     pa = np.append(line_point, 1).reshape(1, -1)
     dr = np.append(line_dir, 0).reshape(1, -1)
-    dot = np.dot(plane_point,plane_normal_vector)
+    dot = np.dot(plane_point, plane_normal_vector)
     pln = np.append(plane_normal_vector, dot).reshape(1, -1)
     res = dr * (pa @ pln.T) - pa * (dr @ pln.T)
     res = res[0]
     res = res/res[-1]
     return res[:3]
+
+
 def intersection(line_point, direction, plane_point, normal_vector):
-        C = np.dot(normal_vector, plane_point)
-        t = C - np.dot(normal_vector, line_point)
-        t = t / np.dot(normal_vector, direction)
-        x0 = direction[0] * t  + line_point[0]
-        y0 = direction[1] * t  + line_point[1]
-        z0 = direction[2] * t  + line_point[2]
-        return np.array([x0, y0, z0])
+    """Intersection Calculation:
+    line : 
+    | x = dx*t+xl
+    | y = dy*t+yl
+    | z = dz*t+zl
+    plane :
+    nx(x-xp)+ny(y-yp)+nz(z-zp)=0
+
+    now we put line equations in plane equation:
+    nx*dx*t + nx*xl + ny*dy*t + ny*yl + nz*dz*t + nz*zl = nx*xp + ny*yp + nz*zp = (C)
+
+    now we can calculate 't':
+    t = [C - (nx*xl+ ny*yl + nz*zl)]/ (nx*dx + ny*dy + nz*dz)
+    """
+    C = np.dot(normal_vector, plane_point)
+    t = C - np.dot(normal_vector, line_point)
+    t = t / np.dot(normal_vector, direction)
+    x0 = direction[0] * t + line_point[0]
+    y0 = direction[1] * t + line_point[1]
+    z0 = direction[2] * t + line_point[2]
+    return np.array([x0, y0, z0])
+
+
 base_dir = os.path.dirname(os.path.realpath(__file__))
 data_folder = os.path.join(base_dir, 'data')
 frame_folder = os.path.join(data_folder, 'monitor_calib_frames')
 os.makedirs(frame_folder, exist_ok=True)
+
 
 def primary_monitor_hw():
     main_monitor = [m for m in get_monitors() if m.is_primary][0]
@@ -71,7 +92,8 @@ if __name__ == "__main__":
     if data_gathering:
         cv2.namedWindow("boom", cv2.WND_PROP_FULLSCREEN)
         # cv2.moveWindow("boom", 1920,0)
-        cv2.setWindowProperty("boom", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+        cv2.setWindowProperty(
+            "boom", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
         for monitor in monitors:
             mW = monitor.width
@@ -134,21 +156,36 @@ if __name__ == "__main__":
             faces = detect_faces_mediapipe(detector, frame)
             face = faces[0]
             estimator.estimate(face, frame)
-            aRay = Line3D(Point3D(face.center * 1e3),
-                          Point3D(face.gaze_vector))
-            rays_3d[str_coords] = rays_3d.get(str_coords, []) + [[face.center * 1e3,face.gaze_vector]]
-            dataset[str_coords] = dataset.get(str_coords, []) + [aRay]
+            aRay = Line3D(Point3D(face.center),
+                          direction_ratio=face.gaze_vector)
+            print("face.center:", face.center)
+            print("face.gaze_vector:", face.gaze_vector)
+            print()
+            rays_3d[str_coords] = rays_3d.get(
+                str_coords, []) + [[face.center, face.gaze_vector]]
+                # dataset[str_coords] = dataset.get(str_coords, []) + [aRay]
+
+        for str_coords in rays_3d.keys():
+            rays_3d[str_coords] = [np.mean(rays_3d[str_coords], 0).tolist()]
+            aRay = Line3D(Point3D(rays_3d[str_coords][0][0]),
+                          direction_ratio=rays_3d[str_coords][0][1])
+            dataset[str_coords] = [aRay]
+
         import pickle
-        with open("dataset.pkl","wb") as f:
-            pickle.dump(dataset,f)
-        fig = plt.figure(figsize=(10,10))
+        with open("dataset.pkl", "wb") as f:
+            pickle.dump(dataset, f)
+        fig = plt.figure(figsize=(10, 10))
         ax = plt.axes(projection='3d')
         for ky in rays_3d.keys():  # ["50,50","960,540"]:
-            for pt3d1,pt3d2 in rays_3d[ky]:
-                pt3d2 = pt3d2 * 1e4 + pt3d1 
-                ax.plot([pt3d1[0],pt3d2[0]], [pt3d1[1],pt3d2[1]], [pt3d1[2],pt3d2[2]], label='parametric curve')
-                ax.scatter(pt3d1[0],pt3d1[1],pt3d1[2])
+            for pt3d1, pt3d2 in rays_3d[ky]:
+                pt3d2 = pt3d2 + pt3d1
+                ax.plot([pt3d1[0], pt3d2[0]], [pt3d1[1], pt3d2[1]], [
+                        pt3d1[2], pt3d2[2]], label='parametric curve')
+                ax.scatter(pt3d1[0], pt3d1[1], pt3d1[2])
 
+        ax.set_xlabel("X")
+        ax.set_ylabel("Y")
+        ax.set_zlabel("Z")
         plt.show()
 
         monitor = monitors[0]
@@ -160,69 +197,96 @@ if __name__ == "__main__":
         corners_3d = (corners / np.array([mW, mH])) * \
             np.array([width_mm, height_mm])
         corners_3d = np.hstack([corners_3d, np.zeros((len(corners_3d), 1))])
-
+        corners_3d = corners_3d / 1e3
         # let's solve this !
         from scipy.spatial.transform import Rotation as R
         from sklearn.metrics import mean_squared_error
         from scipy.optimize import minimize, least_squares
         from skopt import gp_minimize
         from time import time
-        def fit(x):
+        import pygad
+
+        def fit(x, solution_idx=None):
             t1 = time()
             r = R.from_rotvec(x[3:]).as_matrix()
-            t = np.array([2, 2, 2])#np.array(x[:3])
-            if len(t) <3 :
-                t = np.append(t,0)
+            t = np.array(x[:3])
             new_corners = corners_3d @ r + t
             p1, p2, p3 = new_corners[0], new_corners[2], new_corners[4]
             plane = Plane(Point3D(p1), Point3D(p2), Point3D(p3))
-            plane_point = list(map(float,(plane.p1.coordinates)))
+            plane_point = list(map(float, (plane.p1.coordinates)))
             normal_vector = np.array(list(map(float, plane.normal_vector)))
             error = 0
-            for srt_key, aCorner in zip(sorted_keys,new_corners):
+            for srt_key, aCorner in zip(sorted_keys, new_corners):
                 rays = dataset[srt_key]
                 for aRay in rays:
-                    # intersect = np.array(
-                    #     list(map(float, plane.intersection(aRay)[0].coordinates)))
                     apt = (list(map(float, (aRay.points[0].coordinates))))
-                    ldir = list(map(float,(aRay.direction_ratio)))
-                    intersect = intersection(apt,ldir,plane_point,normal_vector)
-                    # print("r:",r)
-                    # print("t:",t)
-                    # print("aray:\n",aRay)
-                    # print("plane:\n",plane)
-                    # print(intersect)
-                    # print(list(map(float,plane.intersection(aRay)[0].coordinates)))
-                    # print("======")
+                    ldir = list(map(float, (aRay.direction_ratio)))
+                    intersect = intersection(
+                        apt, ldir, plane_point, normal_vector)
                     error += np.linalg.norm(np.abs(intersect - aCorner))
             t2 = time()
-            print("error:",error,"time:",t2-t1)
-            return error
-        x0 = np.array([0.5, 0.5, 0.5, -0.1, 0.1, 0.0001])
-        sorted_keys = sorted(list(dataset.keys()),key=lambda x: eval(x))
-        res = minimize(fit, x0, method='L-BFGS-B', options={'ftol': 0.0000001},) #bounds=[(-500, 500),(-500, 500)])
-        # res = least_squares(fit, x0,)
+            print("error:", error, "time:", t2-t1)
+            return - error
+
+        x0 = np.array([-0.18, 0.0, 0.0, 0.0, 0.0, 0.0])
+        sorted_keys = sorted(list(dataset.keys()), key=lambda x: eval(x))
+
+        # res = least_squares(fit, x0, bounds=[(-np.inf, -0.01, -0.01, -0.01, -0.01, -0.01),
+        #                                      (np.inf,   0.01,  0.01,  0.01,  0.01,  0.01)])
+        # res = minimize(fit, x0, method='L-BFGS-B', options={'ftol': 0.0000001}, bounds=[(-np.inf, np.inf), (-np.inf, np.inf), (-0.1, 0.1),
+        #                                                                                 (-np.inf, np.inf),  (-np.inf, np.inf), (-np.inf, np.inf)])
         # res = gp_minimize(fit,                  # the function to minimize
-        #           [(-3.14, 3.14),(-3.14, 3.14),(-3.14, 3.14),(-2e3,2e3),(-2e3,2e3),(-2e3,2e3)],      # the bounds on each dimension of x
-        #           acq_func="EI",      # the acquisition function
-        #           n_calls=1024,         # the number of evaluations of f
-        #           n_initial_points=1024,  # the number of random initialization points
-        #           noise=0.5,       # the noise level (optional)
-        #           random_state=1234)   # the random seed
-        r = R.from_rotvec(res.x[3:]).as_matrix()
-        t = np.array(res.x[:3])
-        if len(t) <3 :
-            t = np.append(t,0)
+        #                   [(-0.1, 0.1), (-0.1, 0.1), (-0.1, 0.1), (-0.1, 0.1),
+        #                    (-0.1, 0.1), (-0.1, 0.1)],      # the bounds on each dimension of x
+        #                   acq_func="EI",      # the acquisition function
+        #                   n_calls=1024,         # the number of evaluations of f
+        #                   n_initial_points=16,  # the number of random initialization points
+        #                   noise=0.5,       # the noise level (optional)
+        #                   random_state=1234)   # the random seed
+        if False:
+            fitness_function = fit
+            num_generations = 2
+            num_parents_mating = 4
+            sol_per_pop = 8
+            num_genes = len(x0)
+            init_range_low = -2
+            init_range_high = 5
+            parent_selection_type = "sss"
+            keep_parents = 1
+            crossover_type = "single_point"
+            mutation_type = "random"
+            mutation_percent_genes = 10
+            ga_instance = pygad.GA(num_generations=num_generations,
+                       num_parents_mating=num_parents_mating,
+                       fitness_func=fitness_function,
+                       sol_per_pop=sol_per_pop,
+                       num_genes=num_genes,
+                       init_range_low=init_range_low,
+                       init_range_high=init_range_high,
+                       parent_selection_type=parent_selection_type,
+                       keep_parents=keep_parents,
+                       crossover_type=crossover_type,
+                       mutation_type=mutation_type,
+                       mutation_percent_genes=mutation_percent_genes)
+            ga_instance.run()
+            solution, solution_fitness, solution_idx = ga_instance.best_solution()
+        xres = x0 #res.x #solution
+        r = R.from_rotvec(xres[3:]).as_matrix()
+        t = np.array(xres[:3])
         new_corners = corners_3d @ r + t
-        fig = plt.figure(figsize=(10,10))
+        fig = plt.figure(figsize=(10, 10))
         ax = plt.axes(projection='3d')
         for ky in rays_3d.keys():
-            for pt3d1,pt3d2 in rays_3d[ky]:
-                pt3d2 = pt3d2 * 1000 + pt3d1 
-                ax.plot([pt3d1[0],pt3d2[0]], [pt3d1[1],pt3d2[1]], [pt3d1[2],pt3d2[2]], label='parametric curve')
-                ax.scatter(pt3d1[0],pt3d1[1],pt3d1[2])
-        ax.scatter(new_corners[:,0],new_corners[:,1],new_corners[:,2])
+            for pt3d1, pt3d2 in rays_3d[ky]:
+                pt3d2 = pt3d2 + pt3d1
+                ax.plot([pt3d1[0], pt3d2[0]], [pt3d1[1], pt3d2[1]], [
+                        pt3d1[2], pt3d2[2]], label='parametric curve')
+                ax.scatter(pt3d1[0], pt3d1[1], pt3d1[2])
+        ax.scatter(new_corners[:, 0], new_corners[:, 1], new_corners[:, 2])
         ax.set_xlabel("x")
         ax.set_ylabel("y")
         ax.set_zlabel("z")
+        ax.set_xlim(-0.8,0.8)
+        ax.set_ylim(-0.8,0.8)
+        ax.set_zlim(-0.8,0.8)
         plt.show()
