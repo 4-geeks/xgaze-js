7b3fbe6ef044b9bf2b5bd82840daad0272c62018
ffletcherr <homayouneemoradi@gmail.com>
2022-03-06 14:45:58 +0330

    intersection method docstring

--- check_intersection.py
+++ check_intersection.py
@@ -4,13 +4,27 @@ from sympy import Plane, Point3D, Line3D, Matrix
 import pickle
 
 def intersection(line_point, direction, plane_point, normal_vector):
-        C = np.dot(normal_vector, plane_point)
-        t = C - np.dot(normal_vector, line_point)
-        t = t / np.dot(normal_vector, direction)
-        x0 = direction[0] * t  + line_point[0]
-        y0 = direction[1] * t  + line_point[1]
-        z0 = direction[2] * t  + line_point[2]
-        return np.array([x0, y0, z0])
+    """Intersection Calculation:
+    line : 
+    | x = dx*t+xl
+    | y = dy*t+yl
+    | z = dz*t+zl
+    plane :
+    nx(x-xp)+ny(y-yp)+nz(z-zp)=0
+
+    now we put line equations in plane equation:
+    nx*dx*t + nx*xl + ny*dy*t + ny*yl + nz*dz*t + nz*zl = nx*xp + ny*yp + nz*zp = (C)
+
+    now we can calculate 't':
+    t = [C - (nx*xl+ ny*yl + nz*zl)]/ (nx*dx + ny*dy + nz*dz)
+    """
+    C = np.dot(normal_vector, plane_point)
+    t = C - np.dot(normal_vector, line_point)
+    t = t / np.dot(normal_vector, direction)
+    x0 = direction[0] * t  + line_point[0]
+    y0 = direction[1] * t  + line_point[1]
+    z0 = direction[2] * t  + line_point[2]
+    return np.array([x0, y0, z0])
 
 def plucker_intersection(line_point, line_dir, plane_point, plane_normal_vector):
     pa = np.append(line_point, 1).reshape(1, -1)
